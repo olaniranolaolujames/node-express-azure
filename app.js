c60ad6c83769c54a1d2d9adf6769c33a094ffe6a
olaoluwa olaniran <olaoluwa olaniran>
2017-12-25 13:48:37 +0100

    first

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var path = require('path');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.post('/add', bodyParser.json(), function(req, res) {
+	var a = req.body.a;
+	var b = req.body.b;
+
+	var sum = a + b;
+
+	res.json(sum);
+});
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(1337);
